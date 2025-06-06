564c845f4c7f8e159188c4fc73927f33cc29f1f8
LouiVi <ramirezdelarosalouisvincent@gmail.com>
2025-06-06 01:17:26 +0000

    Edited with DroidScript

--- IndigoPR.js
+++ IndigoPR.js
@@ -1,28 +1,37 @@
+cfg.Light, cfg.Portrait;
+//, cfg.M/UI;
+app.LoadPlugin( "Utils" );
 function OnStart()
 {
+	utils = app.CreateUtils();
+	app.SetStatusBarColor(  utils.RandomHexColor());
+	app.SetNavBarColor( utils.RandomHexColor());
+	//app.WriteFile( app.GetAppPath()+"/u.js", utils.GetSource());
+	app.ShowProgress( "Loading ...", "Solid");
 	lay = app.CreateLayout( "Linear", "VCenter,FillXY" )
 	web= app.CreateWebView( 1, 1 );
 	lay.AddChild( web );
 	app.AddLayout( lay );
-		if(!app.FileExists( app.GetAppPath()+"/path.txt" )) {
+	utils.SetTheme(utils.RandomHexColor());
+	app.SetStatusBarColor(  utils.RandomHexColor());
+	app.SetNavBarColor( utils.RandomHexColor());
+	if(!app.FileExists( app.GetAppPath()+"/path.txt" )) {
 		GetPath();
 	}else{
-	dload_OnDownload(app.GetAppPath()+"/path.txt");
-	/*adrs = app.ReadFile( path ) ;
-		theme = app.LoadText( "theme", "a", "theme.txt" );
-    app.HttpRequest( "GET", adrs.replace("[L]", theme), null, null, handleReply );*/
-    
+		dload_OnDownload(app.GetAppPath()+"/path.txt");
 	}
 }
 
 function handleReply( error, reply )
 {
+		app.ShowProgress( "Loading ..." );
     if( error ) alert( reply );
     else
     {
     app.WriteFile( "source.html", reply );
     web.LoadHtml( reply,adrs.replace("[L]",theme) );
     }
+    app.HideProgress();
 }
 
 function ChangeTheme(theme)
@@ -41,6 +50,7 @@ function GetPath()
     dload.SetOnComplete( dload_OnComplete );
     dload.SetOnDownload( dload_OnDownload );
     dload.Download( srcFileUrl, targetDir );
+    app.HideProgress();
 }
 
 function dload_OnComplete()
@@ -53,4 +63,5 @@ function dload_OnDownload(path)
 		adrs = app.ReadFile( path ) ;
 		theme = app.LoadText( "theme", "a", "theme.txt" );
     app.HttpRequest( "GET", adrs.replace("[L]", theme), null, null, handleReply );
+    app.HideProgress();
 }
